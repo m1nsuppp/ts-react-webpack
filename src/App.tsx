83a0a74c70a84707b2c25baccf4a11abfc56beb7
m1nsuppp <m1nsuppp@gmail.com>
2022-12-04 23:34:57 +0900

    style: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 export default function App () {
+  const [content, setContent] = useState<string>('this is boilerplate.');
   return (
     <div>
       Hello, World!
-      <h1>this is BOILERPLATE</h1>
+      <h1 onClick={() => setContent('Keep it on the down low.')}>{content}</h1>
     </div>
   );
 }
